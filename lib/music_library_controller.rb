42a3435eab9349a45b891162d6b06b146fff310f
Yukiji Nakatsuka <lunarbase@outlook.com>
2019-01-17 18:33:20 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -11,7 +11,6 @@ class MusicLibraryController
     while user_input != "exit"
     
       puts "Welcome to your music library!"
-      user_input = gets.chomp
       puts "To list all of your songs, enter 'list songs'."
       puts "To list all of the artists in your library, enter 'list artists'."
       puts "To list all of the genres in your library, enter 'list genres'."
@@ -21,22 +20,24 @@ class MusicLibraryController
       puts "To quit, type 'exit'."
       puts "What would you like to do?"
     
-    case user_input
-    when "list songs"
+      user_input = gets.chomp
+      case user_input
+      when "list songs"
       
-    when "list artists"
+      when "list artists"
       
-    when "list genres"
+      when "list genres"
       
-    when "list artist"
+      when "list artist"
         
-    when "list genre"
+      when "list genre"
           
-    when "play song"
+      when "play song"
   
-    when "exit"
+      when "exit"
     
-    end 
+      end 
+    end
     
   end 
   
