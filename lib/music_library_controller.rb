e78f7ab2e808fb2983f8f43468359a1a9357e1e6
Yukiji Nakatsuka <lunarbase@outlook.com>
2019-01-17 18:32:54 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -12,23 +12,29 @@ class MusicLibraryController
     
       puts "Welcome to your music library!"
       user_input = gets.chomp
-      case user_input
       puts "To list all of your songs, enter 'list songs'."
-      when "list songs"
       puts "To list all of the artists in your library, enter 'list artists'."
-      when "list artists"
+      puts "To list all of the genres in your library, enter 'list genres'."
       puts "To list all of the songs by a particular artist, enter 'list artist'."
-      when "list genres"
       puts "To list all of the songs of a particular genre, enter 'list genre'."
-      when "list artist"
       puts "To play a song, enter 'play song'."
-      when "list genre"
       puts "To quit, type 'exit'."
-      when "exit"
       puts "What would you like to do?"
     
+    case user_input
+    when "list songs"
+      
+    when "list artists"
+      
+    when "list genres"
+      
+    when "list artist"
+        
+    when "list genre"
+          
+    when "play song"
   
-    
+    when "exit"
     
     end 
     
