0499d15d4795abb4fd9c3cc2d31ceadef94123a1
Yukiji Nakatsuka <lunarbase@outlook.com>
2019-01-17 23:15:29 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -76,8 +76,6 @@ class MusicLibraryController
           sorted_lists.each.with_index(1) do |list, index|
             puts "#{index}. #{list[0]} - #{list[1]}"
           end
-        else 
-          puts "Please enter the name of an artist:"
         end
       end
     end 
