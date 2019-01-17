5e7d89ee3ac3a5ad5d36607f6bd11dedf8b186af
Yukiji Nakatsuka <lunarbase@outlook.com>
2019-01-17 18:53:23 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -41,7 +41,7 @@ class MusicLibraryController
     end
     
     def list_songs
-      binding.pry
+      #binding.pry
       lists = Song.all.map {|song| song.name}.sort
       lists.each.with_index(1) {|list, idx| puts "#{idx}. #{list}" }
     end
