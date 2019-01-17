c5e2675965250461c5882d22813e497fd706e314
Yukiji Nakatsuka <lunarbase@outlook.com>
2019-01-17 18:30:36 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -13,8 +13,7 @@ class MusicLibraryController
       puts "Welcome to your music library!"
       user_input = gets.chomp
       case user_input
-      
-      puts "To list all of your songs, enter 'list songs'."
+        puts "To list all of your songs, enter 'list songs'."
       puts "To list all of the artists in your library, enter 'list artists'."
       puts "To list all of the genres in your library, enter 'list genres'."
       puts "To list all of the songs by a particular artist, enter 'list artist'."
