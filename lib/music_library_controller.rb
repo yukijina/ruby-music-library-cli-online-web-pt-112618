384a1e78e5ca12f344451b162ad3b94b1d943e46
Yukiji Nakatsuka <lunarbase@outlook.com>
2019-01-17 18:30:04 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -21,8 +21,7 @@ class MusicLibraryController
       puts "To quit, type 'exit'."
       puts "What would you like to do?"
     
-    case user_input
-    when "list songs"
+   
       
     when "list artists"
       
