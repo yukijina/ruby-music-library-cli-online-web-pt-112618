f0b12c164e53e592cc497ece37c12fd1cf151e7f
Yukiji Nakatsuka <lunarbase@outlook.com>
2019-01-17 18:58:04 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -45,6 +45,7 @@ class MusicLibraryController
       lists = Song.all.each do |song| 
         song.name.sort.each.with_index(1) do |list, index|
           puts "#{index}. #{song.artist} - #{song.name} - #{song.genre}"
+        end
       end
       #lists.each.with_index(1) {|list, idx| puts "#{idx}. #{list}" }
     end
