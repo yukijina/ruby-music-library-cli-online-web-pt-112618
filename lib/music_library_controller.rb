8c679c7b98184f4741ccadd9cde7e7f4df8b126a
Yukiji Nakatsuka <lunarbase@outlook.com>
2019-01-18 01:52:04 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -29,7 +29,7 @@ class MusicLibraryController
       when "list genres"
         list_genres
       when "list artist"
-          
+        list_songs_by_artist 
       when "list genre"
           
       when "play song"
