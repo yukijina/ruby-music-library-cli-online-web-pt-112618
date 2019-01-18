2074e26fd6c5faf5084837b073a1148d79443606
Yukiji Nakatsuka <lunarbase@outlook.com>
2019-01-18 01:14:48 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -48,7 +48,7 @@ class MusicLibraryController
       
       sorted_lists = lists.each_slice(3).map {|list| list}.sort
       sorted_lists.each.with_index(1) do |list, index|
-         "#{index}. #{list[1]} - #{list[0]} - #{list[2]}"
+        puts "#{index}. #{list[1]} - #{list[0]} - #{list[2]}"
       end
       
     end
